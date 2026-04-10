27d2e763914966783233c69004d0b49fc265dee5
with-Khilraj <khilstha75@gmail.com>
2026-04-10 14:56:40 +0545

    feat(profile): add two field `bio` and `avatar` for profile section

--- server/features/user/user.model.js
+++ server/features/user/user.model.js
@@ -6,8 +6,9 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    // isOnline: { type: Boolean, default: false },
     lastActive: { type: Date, default: null },
+    bio: { type: String, default: "" },
+    avatar: { type: String, default: "" },
     isEmailVerified: {
       type: Boolean,
       default: false,

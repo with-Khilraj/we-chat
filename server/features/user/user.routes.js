d05eaba2fdcf27ad0bbb241bc708b5ba95c94e1b
with-Khilraj <khilstha75@gmail.com>
2026-04-10 14:58:50 +0545

    add routes

--- server/features/user/user.routes.js
+++ server/features/user/user.routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./user.controller");
 const verifyAccessToken = require("../../common/middlewares/authMiddleware");
+const { upload, uploadErrorHandler } = require("../../common/middlewares/upload");
 
 // All user routes are protected by default for now
 router.use(verifyAccessToken);
 
 // Get current user profile
 router.get("/profile", userController.getProfile);
+
+// Update current user profile
+router.put("/profile", upload.single('avatar'), uploadErrorHandler, userController.updateProfile);
 
 // Get all verified users except requester
 router.get("/all", userController.getUsers);
